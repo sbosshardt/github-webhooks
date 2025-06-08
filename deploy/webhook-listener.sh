1ce2ffd902ff72357d04589607f0ebc9170ae9e3
Samuel Bosshardt <5762452+sbosshardt@users.noreply.github.com>
2025-06-07 19:01:49 -0700

    fix: Added sudo for restarting webhook-listener.

--- deploy/webhook-listener.sh
+++ deploy/webhook-listener.sh
@@ -5,7 +5,7 @@ cd /home/pm2/webhook-listener || exit 1
 git fetch
 git pull origin master
 
-systemctl restart webhook-listener
+sudo /bin/systemctl restart webhook-listener
 # You should set up this user to run the above command without a password. i.e.
 # sudo visudo
 # pm2 ALL=(ALL) NOPASSWD: /bin/systemctl restart webhook-listener
